33a764c4f791cfc9ed68f984d35ce2c04fb305ab
Daniel Miedema <danielmiedema@me.com>
2013-02-10 14:30:38 -0800

    Attempting to find solution. commented out a decent idea perhaps

--- diet.hs
+++ diet.hs
@@ -13,26 +13,18 @@ main = do
 	print listOfActivites
 	print values
 
-
-
-
 createTuple :: String -> (String, String)
 createTuple string = splitAt (head (elemIndices ' ' string)) string
 
 extractValues :: String -> Int
 extractValues string = read (snd (splitAt (head (elemIndices ' ' string)) string)) :: Int
 
---getValues :: [(String, String)] -> [Int]
---getValues listOfTuples = 
---	x = head listOfTuples
---	xs = tail listOfTuples
+findSolution :: ([Int], [Int]) -> [Int]
+findSolution tuple = 
+	let positives = fst tuple
+	let negatives = snd tuple
 
 
+--filter (== 0) (snd $ mapAccumL (\ x y -> (x,x+y)) 2 [1,2,3,-3,-4,-1])
 
---findSolution :: [Int] -> [Int]
---findSolution activities = 
-
--- A* - each value is a node, goal is 0
---partition (>0) <list>
-
-	--read $ snd $ head [("something", "-14")] :: Int
+-- A* - each value is a node, goal is 0	
