a793336407e3c61f8d6c72f4802ade857d13b6ea
Daniel Miedema <danielmiedema@me.com>
2013-02-18 14:10:22 -0800

    Added check opposite function that does Data.List.nub to remove duplicates. checks length against un-nub'd list to see if there is a duplicated value somewhere in the list.

--- diet.hs
+++ diet.hs
@@ -14,9 +14,9 @@ main = do
 		activity <- getLine
 		return activity)
 	let nodes = fromList (map createNode (map createTuple (List.sort activities)))
-	--print activities
 	let paritionedValues = List.partition (>0) (map getValues activities)
 	print paritionedValues
+	print (checkOppositeValues paritionedValues)
 	--print nodes
 	--print (Seq.length nodes)
 
@@ -29,5 +29,29 @@ getValues split = read (snd (createTuple split)) :: Int
 createNode :: (String,String) -> Node
 createNode split =  Node {activity = (fst split), value = (read (snd (split)) :: Int), used = False}
 
+checkOppositeValues :: ([Int], [Int]) -> Bool
+checkOppositeValues tuple = 
+	Seq.length (Seq.fromList(fst tuple ++ (map (abs) (snd tuple)))) == 
+		Seq.length (Seq.fromList( List.nub (fst tuple ++ (map (abs) (snd tuple)))))
+
+
+
+
+	-- Merge lists and covert all number to positive numbers
+	-- fst tuplelist ++ (map (abs) (snd tuplelist))
 
 -- A* - each value is a node, goal is 0	
+
+-- Holy derp.
+-- map (== (read (\x -> map (head) (fst tuplelist))::Int)) (fst tuplelist  ++ (map (abs) (snd tuplelist )))
+		{-
+		The lambda expression `\ x -> map (head) (fst tuplelist)'
+		    has one argument,
+		    but its type `String' has none
+		    In the first argument of `read', namely
+		      `(\ x -> map (head) (fst tuplelist))'
+		    In the second argument of `(==)', namely
+		      `(read (\ x -> map (head) (fst tuplelist)) :: Int)'
+		    In the first argument of `map', namely
+		      `(== (read (\ x -> map (head) (fst tuplelist)) :: Int))'
+		-}
